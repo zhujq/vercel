1a15e6680667ff5989a0f8964203ca6889b7a49d
zhujq <=>
2022-04-26 19:36:58 +0800

    v0.1

--- api/showheader.go
+++ api/showheader.go
@@ -7,13 +7,11 @@ import (
 
 func DisplayHeadersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Method: %s URL: %s Protocol: %s \n", r.Method, r.URL, r.Proto)
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
 	// 遍历所有请求头
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
 	}
 
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-	// 通过 Key 获取指定请求头的值
-	fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q", r.Header["Accept"])
 }
