4bb1c3fc30cc2a4bf17a8ca62c0bf284340f8993
zhujq <=>
2022-04-28 00:45:11 +0800

    phonequery

--- api/phonequery.go
+++ api/phonequery.go
@@ -1,21 +1,22 @@
 package handler
 
 import (
-	"flag"
-//	"fmt"
+
+	//	"fmt"
+	"encoding/json"
 	"log"
-	"os"
 	"net/http"
 	"strings"
-	"github.com/zhujq/phonedata"
+
+	"./phonedata"
 )
 
 type ResBody struct {
-	Status      string `json:"status"`
-	PhoneNum   string `json:"PhoneNum"`
-	Province     string `json:"Province"`
-	City   string `json:"City "`
-	ZipCode    string `json:"ZipCode"`
+	Status   string `json:"status"`
+	PhoneNum string `json:"PhoneNum"`
+	Province string `json:"Province"`
+	City     string `json:"City "`
+	ZipCode  string `json:"ZipCode"`
 	AreaZone string `json:"AreaZone"`
 	CardType string `json:"CardType"`
 }
@@ -26,33 +27,33 @@ func Getphone(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if phonenum == "" {
-		w.Write([]byte("欢迎使用中国电话号码查询系统，请在网址后输入 /?phonenum=电话号码 查询!")
+		w.Write([]byte("欢迎使用中国电话号码查询系统，请在网址后输入 /?phonenum=电话号码 查询!"))
 	} else {
-		
+
 		var message ResBody
 		message.PhoneNum = phonenum
 
-		if strings.HasPrefix(phonenum,"86"){
+		if strings.HasPrefix(phonenum, "86") {
 
-			phonenum = strings.TrimPrefix(phonenum,"86")
+			phonenum = strings.TrimPrefix(phonenum, "86")
 
 		}
 
 		phoneresult, err := phonedata.Find(phonenum)
 
-		if err != nil {	
-			log.Println("error:", err)	
-			w.Write([]byte("查询不到你输入的中国号码信息!")
+		if err != nil {
+			log.Println("error:", err)
+			w.Write([]byte("查询不到你输入的中国号码信息!"))
 			return
 		}
-	
+
 		message.Status = "success"
 		message.Province = phoneresult.Province
 		message.City = phoneresult.City
 		message.ZipCode = phoneresult.ZipCode
 		message.AreaZone = phoneresult.AreaZone
 		message.CardType = phoneresult.CardType
-		js, _ := json.Marshal()
+		js, _ := json.Marshal(message)
 		w.Write(js)
 	}
 
