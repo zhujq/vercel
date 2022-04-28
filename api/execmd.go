63eddf0812174122e0e3430e98949377084ed1e6
zhujq <=>
2022-04-28 18:57:40 +0800

    add_wechat

--- api/execmd.go
+++ api/execmd.go
@@ -11,30 +11,42 @@ func Execmd(w http.ResponseWriter, r *http.Request) {
 	whoami, _ := cmd.CombinedOutput()
 	fmt.Fprintf(w, "uname -a \n ")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
+
+	cmd := exec.Command("bash", "-c", "ls -al ./")
+	whoami, _ := cmd.CombinedOutput()
+	fmt.Fprintf(w, "ls -al ./ \n ")
+	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 	cmd = exec.Command("bash", "-c", "lsb_release -a")
 	whoami, _ = cmd.CombinedOutput()
 	fmt.Fprintf(w, "lsb_release -a \n")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 	cmd = exec.Command("bash", "-c", "free -h")
 	whoami, _ = cmd.CombinedOutput()
 	fmt.Fprintf(w, "free -h \n")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 	cmd = exec.Command("bash", "-c", "df -Th")
 	whoami, _ = cmd.CombinedOutput()
 	fmt.Fprintf(w, "df -Th \n ")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 	cmd = exec.Command("bash", "-c", "cat /proc/cpuinfo")
 	whoami, _ = cmd.CombinedOutput()
 	fmt.Fprintf(w, "cat /proc/cpuinfo \n")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 	cmd = exec.Command("bash", "-c", "netstat -atunp")
 	whoami, _ = cmd.CombinedOutput()
 	fmt.Fprintf(w, "netstat -atunp \n")
 	fmt.Fprintf(w, string(whoami))
+	fmt.Fprintf(w, "\n")
 
 }
