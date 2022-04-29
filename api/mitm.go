f2a766634787b7e3039a5111321e1c08aadcd5c6
zhujq <=>
2022-04-29 15:26:43 +0800

    mitm

--- api/mitm.go
+++ api/mitm.go
@@ -1,17 +1,29 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type realbody struct {
+	method  string `json:"method"`
+	url     string `json:"url"`
+	headers string `json:"headers"`
+	cookies string `json:"cookies"`
+	params  string `json:"params"`
+	data    string `json:"data"`
+}
+
 func Proxyweb(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.Method)
 	if r.Method == "POST" {
 		rbody, _ := ioutil.ReadAll(r.Body)
-		log.Println(rbody)
+		rb := realbody{}
+		json.Unmarshal([]byte(rbody), &rb)
+		log.Println(rb)
 		fmt.Print(w, "hello")
 
 	}
