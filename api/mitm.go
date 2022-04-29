48e6b814c648c566d8aa6a3981a9cbe2cba1f40b
zhujq <=>
2022-04-30 01:21:01 +0800

    mitm

--- api/mitm.go
+++ api/mitm.go
@@ -19,6 +19,13 @@ type realbody struct {
 	Data    []byte            `json:"data"`
 }
 
+type rspbody struct {
+	IsBase64Encoded bool
+	StatusCode      int
+	Headers         map[string]string
+	Data            []byte
+}
+
 func Proxyweb(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "POST" {
@@ -32,20 +39,19 @@ func Proxyweb(w http.ResponseWriter, r *http.Request) {
 		log.Println(rb.Url)
 
 		log.Println("Decoding headers:")
-		/*	for k, v := range *rb.Headers {
-				log.Println(k, "=", v)
-			}
-		*/
-		log.Println(rb.Headers)
+		for k, v := range rb.Headers {
+			log.Println(k, "=", v)
+		}
+
 		log.Println("Decoding cookies:")
-		log.Println(rb.Cookies)
+		for k, v := range rb.Cookies {
+			log.Println(k, "=", v)
+		}
 
 		log.Println("Decoding url params:")
-		/*	for k, v := range rb.Params {
-				log.Println(k, "=", v)
-			}
-		*/
-		log.Println(rb.Params)
+		for k, v := range rb.Params {
+			log.Println(k, "=", v)
+		}
 
 		reqbody := make([]byte, base64.StdEncoding.DecodedLen(len(rb.Data)))
 
@@ -53,20 +59,30 @@ func Proxyweb(w http.ResponseWriter, r *http.Request) {
 		reqbody = reqbody[:n]
 		client := &http.Client{}
 		req, err := http.NewRequest(rb.Method, rb.Url, bytes.NewReader(reqbody))
-		//	req.Header = *rb.Headers
-		//	req.AddCookie(rb.Cookies)
+		for k, v := range rb.Headers {
+			req.Header.Add(k, v)
+		}
+
 		resp, err := client.Do(req)
 		log.Println("Getting result:")
 		log.Println(resp.Status)
 		log.Println(resp)
 
+		rspcontent := rspbody{}
+
 		buff := new(bytes.Buffer)
 		binary.Write(buff, binary.BigEndian, resp)
 		rsp := buff.Bytes()
-		dst := make([]byte, base64.StdEncoding.EncodedLen(len(rsp)))
-		base64.StdEncoding.Encode(dst, rsp)
-		w.WriteHeader(200)
-		w.Write(buff.Bytes())
+		//	dst := make([]byte, base64.StdEncoding.EncodedLen(len(rsp)))
+		base64.StdEncoding.Encode(rspcontent.Data, rsp)
+
+		rspcontent.IsBase64Encoded = false
+		rspcontent.StatusCode = 200
+
+		buff2 := new(bytes.Buffer)
+		binary.Write(buff2, binary.BigEndian, rspcontent)
+
+		w.Write(buff2.Bytes())
 
 	}
 
