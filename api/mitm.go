208708615c1be184033404a832161e8025daf826
zhujq <=>
2022-04-29 14:54:45 +0800

    mitm

--- api/mitm.go
+++ api/mitm.go
@@ -3,14 +3,15 @@ package handler
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func Proxyweb(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "post" {
 		rbody, _ := ioutil.ReadAll(r.Body)
-
-		fmt.Print(w, "%s", rbody)
+		log.Println(rbody)
+		fmt.Print(w, "hello")
 
 	}
 
