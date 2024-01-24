43507befe5dc84d8f72d5d2f1086138e14e65f4d
zhujq <=>
2024-01-24 16:53:04 +0800

    sip-youhua

--- api/myinfo.go
+++ api/myinfo.go
@@ -4,13 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
+
+func ClientPublicIP(r *http.Request) string {
+	var ip string
+
+	if ip = strings.TrimSpace(r.Header.Get("Cf-Connecting-Ip")); ip != "" {
+		return ip
+	}
+
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		if ip = strings.TrimSpace(ip); ip != "" {
+			return ip
+		}
+	}
+
+	if ip = strings.TrimSpace(r.Header.Get("X-Real-Ip")); ip != "" {
+		return ip
+	}
+
+	if ip = RemoteIP(r); ip != "" {
+		return ip
+	}
+
+	return ""
+}
 
 func MyInfo(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
-	resp["ip"] = r.RemoteAddr
+	resp["ip"] = ClientPublicIP(r)
 	resp["user-agent"] = r.UserAgent()
 	resp["accept-language"] = r.Header.Get("Accept-Language")
 	jsonResp, err := json.Marshal(resp)
